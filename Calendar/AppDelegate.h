d5238a920655b95790c96be5130f113cff8addf2
liuli <lllxy003@163.com>
2018-07-02 14:40:21 +0800

    Initial Commit

---
+++ Calendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calendar
+//
+//  Created by 萧珏 on 2018/7/2.
+//  Copyright © 2018年 small apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calendar
+//
+//  Created by 萧珏 on 2018/7/2.
+//  Copyright © 2018年 small apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
